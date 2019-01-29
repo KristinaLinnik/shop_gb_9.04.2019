4145458cf83ff967346e4f6b0c2189314daf242d
kris <func.kris@gmail.com>
2019-01-29 11:57:34 +0100

    first commit

---
+++ data/js/main.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+    $('.collapse').collapse()
+    $('#user-rating-form').on('change','[name="rating"]',function(){
+        $('#selected-rating').text($('[name="rating"]:checked').val());
+    });
+
+});
