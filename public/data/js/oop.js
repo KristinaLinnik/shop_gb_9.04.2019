e76feaad5340ecf1ae93b33049caa78b2c5cd691
kris <func.kris@gmail.com>
2019-04-09 16:53:46 +0200

    9.04.2019//16:54

--- public/data/js/oop.js
+++ public/data/js/oop.js
@@ -1,49 +1,46 @@
 'use strict';
+window.onload = function () {
+
+    cartGenerate();
+
+};
 
 //Запрос на сервер
 function sendRequest(url) {
     return fetch(url).then((response) => response.json());
 }
 
-// function sendRequest(url) {
-// return new Promise(
-//     ((resolve) => {
-//         const xhr = new XMLHttpRequest();
-//         xhr.open('GET', url); // настройка запроса
-//         xhr.send();
-//         xhr.addEventListener("load", function() {
-//             if (xhr.readyState === XMLHttpRequest.DONE) {
-//                 if(xhr.status === 200){
-//                     resolve(JSON.parse(xhr.responseText));
-//                 }
-//             }else{
-//                 reject(console.log("Request failed: " + xhr.statusText))
-//             }
-//         });
-//
-//     })
-// )
-// }
 const API_URL = "http://localhost:3000";
 
 //Отрисовка еденицы товара
 class Item {
-    constructor(url, title, price, id) {
+    constructor(url, title, price, id, color, size) {
         this.url = url;
         this.title = title;
         this.price = price;
         this.id = id;
+        this.color = color;
+        this.size = size;
     }
 
     render() {
         return ` <li class="shop-item">
             <a class="shop-item-link" href = 'single-page.html' style="background-image: url(${this.url})"></a>
-            <a class="add-to-card" href="#" data-id="${this.id}" >Add to card</a>
+            <a class="add-to-card" href="#" data-id="${this.id}" data-title="${this.title}" data-price="${this.price}" data-url="${this.url}" data-color="${this.color}" data-size="${this.size}">Add to card</a>
             <a href="single-page.html" class="item-brand">${this.title}</a>
              <p class="item-price pink">${this.price}</p>
         </li>`
     }
 }
+
+const searchBtn = document.querySelector('.btn-search');
+const searchText = document.querySelector('#filter');
+searchBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    list.filterItems(searchText.value);
+    document.querySelector(".shop-item-wrap").innerHTML = list.render();
+
+});
 
 //отрисовка каталога
 class ItemsList {
@@ -54,13 +51,32 @@ class ItemsList {
     fetchItems() {
         return sendRequest(`${API_URL}/products`).then(
             (catalog) => {
-                this.catalog = catalog.map(item => new Item(item.url, item.title, item.price, item.id));
+                this.catalog = catalog.map(item => new Item(item.url, item.title, item.price, item.id, item.color, item.size));
+                this.filteredItems = this.catalog;
+
             });
     }
 
     render() {
-        const itemsHtmls = this.catalog.map(Item => Item.render());
+        let itemsHtmls = this.filteredItems.map(Item => Item.render());
         return itemsHtmls.join("");
+        // if(searchText.value === ' ') {
+        // console.log('input is empty');
+        // } else {
+        // itemsHtmls = this.filteredItems.map(Item => Item.render());
+        // }
+    }
+
+    filterItems(value) {
+        const regexp = new RegExp(value, 'i');
+        this.filteredItems = this.catalog.filter((item) => regexp.test(item.title));
+        // this.filteredItems.forEach(el => {
+        // if(el.title === item.title){
+        // console.log(el);
+        // return(el)
+        // }
+        // })
+        // console.log(this.filteredItems.title)
     }
 }
 
@@ -74,12 +90,8 @@ list.fetchItems().then(
 //отрисовка корзины
 class Basket extends Item {
     constructor(url, title, price, color, size, quantity, id) {
-        super(url, title, price, id);
-        this.color = color;
-        this.size = size;
+        super(url, title, price, id, size, color);
         this.quantity = quantity;
-        this.sum = quantity * price;
-
     }
 
     renderCart() {
@@ -106,9 +118,9 @@ class Basket extends Item {
                                 FREE
                             </div>
                             <div class="subtotal">
-                                ${this.sum}
+                               $ ${this.quantity * this.price}
                             </div>
-                            <div class="delete-good-btn action" data-id="${this.id}">
+                            <div class="delete-good-btn action" data-id="${this.id}" data-quantity="${this.quantity}">
                                 <i class="fas fa-times-circle"></i>
                             </div>
                         </div>`
@@ -123,51 +135,126 @@ class CartList {
     fetchGoods() {
         return sendRequest(`${API_URL}/cart`).then(
             (cartCatalog) => {
-                this.cartCatalog = cartCatalog.map(good => new Basket(good.url, good.title, good.price, good.color, good.size, good.quantity));
+                this.cartCatalog = cartCatalog.map(good => new Basket(good.url, good.title, good.price, good.color, good.size, good.quantity, good.id));
+
             }
         );
     }
 
     calculateSum() {
-        return this.cartCatalog.reduce((acc, item) => acc + item.price, 0);
+        return this.cartCatalog.reduce((acc, item) => acc + (+item.price * item.quantity), 0);
     }
 
     renderCart() {
         const goodsHtmls = this.cartCatalog.map(good => good.renderCart());
         return goodsHtmls.join('');
     }
+
+    addToCart(data) {
+        if (data.quantity > 1) {
+            fetch(`/cart/${data.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({quantity: data.quantity}),
+            })
+                .then((response) => response.json())
+                .then(() => cartGenerate())
+        } else {
+            fetch('/cart', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(...data, quantity),
+            })
+                .then((response) => response.json())
+                .then(cart.fetchGoods())
+                .then(() => cartGenerate())
+        }
+
+    }
+
+    deleteGood(et, dataSet) {
+        if (dataSet.quantity >= 1) {
+            fetch(`/cart/${dataSet.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({quantity: dataSet.quantity})
+            })
+                .then((response) => response.json())
+                .then(() => cartGenerate())
+                .then(() => delete et.dataset)
+
+
+        } else {
+            fetch(`/cart/${dataSet.id}`, {method: 'delete'})
+                .then(() => {
+                    cartWrapper.removeChild(et.parentElement);
+                })
+                .then(() => cartGenerate())
+                .then(() => delete et.dataset)
+        }
+    }
 }
 
 const cart = new CartList();
-cart.fetchGoods().then(
-    () => {
-        const $catalogWrap = document.querySelector('.shop-item-wrap');
-        const url = '/db.json/prducts/url'
 
-        $catalogWrap.addEventListener('click', (event) => {
-                event.preventDefault();
-                if (event.target.classList.contains('add-to-card')) {
-                    console.log('111');
-                    fetch('/cart', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({url, title, price, color, size, quantity}),
-                    }).then((response) => response.json())
-                        .then((cart) => {
-                            document.querySelector('.rows-wrap').innerHTML = cart.renderCart();
-                            document.querySelector('#all-total').innerHTML = cart.calculateSum();
-                        })
-                }
+function cartGenerate() {
+    cart.fetchGoods()
+        .then(
+            () => {
+                document.querySelector('.rows-wrap').innerHTML = cart.renderCart();
+                document.querySelector('#all-total').innerHTML = '$ ' + cart.calculateSum();
+                const $totalsum = document.querySelectorAll('.total-sum');
+                $totalsum.forEach((item) => item.innerHTML = '$ ' + (cart.calculateSum() - (cart.calculateSum() * 0.1)));
             }
-        );
+        )
 
-        const $totalsum = document.querySelectorAll('.total-sum');
-        $totalsum.forEach((item) => item.innerHTML = cart.calculateSum() - (cart.calculateSum() * 0.1));
+}
+
+const $catalogWrap = document.querySelector('.shop-item-wrap');
+const url = '/db.json/products';
+
+$catalogWrap.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target.classList.contains('add-to-card')) {
+            const data = event.target.dataset;
+            const data2 = quan(data);
+            cart.addToCart(data2);
+        }
     }
 );
 
+function quan(data) {
+    if (!data["quantity"] || data["quantity"] <= 0) {
+        data["quantity"] = 1;
+    } else if (data["quantity"] >= 1) {
+        data["quantity"]++
+    }
+    return data
+}
+
+
+const cartWrapper = document.querySelector('#parentOfCart');
+cartWrapper.addEventListener('click', (event) => {
+        const et = event.target.parentElement;
+        let dataSet = event.target.parentElement.dataset;
+        const goodInCatalog = document.querySelectorAll('.add-to-card');
+        for (let i = 0; i < goodInCatalog.length; i++) {
+            if (goodInCatalog[i].dataset.id === dataSet.id) {
+                parseInt(goodInCatalog[i].dataset.quantity--);
+            }
+        }
+        if (event.target.parentElement.classList.contains('delete-good-btn')) {
+            dataSet.quantity = parseInt(dataSet.quantity) - 1;
+            cart.deleteGood(et, dataSet);
+        }
+    }
+);
 
 
 
